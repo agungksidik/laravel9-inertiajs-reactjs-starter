4c906eddf2f9200c90c4e2a5b113039d3e8db222
agungksidik <agungksidik@gmail.com>
2022-02-13 00:09:16 +0700

    Add route middleware

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,20 @@
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
-Route::get('/dashboard', DashboardController::class);
 
-Route::get('/login', [LoginController::class, 'create']);
-Route::post('/login', [LoginController::class, 'store']);
-Route::post('/logout', [LoginController::class, 'destroy']);
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', DashboardController::class);
+    Route::post('logout', [LoginController::class, 'destroy']);
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('login', [LoginController::class, 'create'])->name('login');
+    Route::post('login', [LoginController::class, 'store']);
+    
+    Route::get('register', [RegisterController::class, 'create']);
+    Route::post('register', [RegisterController::class, 'store']);
+});
