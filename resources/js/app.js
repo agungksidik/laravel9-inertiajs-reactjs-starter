22246ba5adfa390d4be9253041bdd68abe5bca32
agungksidik <agungksidik@gmail.com>
2022-02-19 17:48:52 +0700

    Add progress indicator

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createInertiaApp } from '@inertiajs/inertia-react'
+import { InertiaProgress } from '@inertiajs/progress'
 import 'bootstrap'
 
 createInertiaApp({
@@ -8,4 +9,19 @@ createInertiaApp({
   setup({ el, App, props }) {
     render(<App {...props} />, el)
   },
+})
+
+InertiaProgress.init({
+  // The delay after which the progress bar will
+  // appear during navigation, in milliseconds.
+  delay: 0,
+
+  // The color of the progress bar.
+  color: 'orange',
+
+  // Whether to include the default NProgress styles.
+  includeCSS: true,
+
+  // Whether the NProgress spinner will be shown.
+  showSpinner: true,
 })
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .react()
-    .postCss('resources/css/app.css', 'public/css', [
+    .sass('resources/sass/app.sass', 'public/css', [
         //
     ]);
 
